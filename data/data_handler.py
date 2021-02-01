eaf386131973720187d68f129761b9616dbfd6eb
jakob engebretsen <jakob.fougner.engebretsen@gmail.com>
2021-02-01 17:48:08 +0100

    Median-metode for periodevelger

--- data/data_handler.py
+++ data/data_handler.py
@@ -1,1 +1,50 @@
 # script connecting src and data folder.
+import datetime
+import pandas as pd
+import numpy as np
+
+
+def get_data(from_date, to_date, column_list):  # dates on form "dd.mm.yyyy", columns in list ["System Price", ...]
+    print("--Retrieving data from {} to {}. Columns: {} --\n".format(from_date, to_date, ", ".join(column_list)))
+    valid_dates = check_valid_dates(from_date, to_date)
+    if valid_dates:
+        first_row_in_data = datetime.datetime.strptime("01.01.2014", '%d.%m.%Y')
+        first_date = datetime.datetime.strptime(from_date, '%d.%m.%Y')
+        last_date = datetime.datetime.strptime(to_date, '%d.%m.%Y')
+        difference = first_date - first_row_in_data
+        years = difference.days // 365
+        hours_less = years - 1
+        skip_rows_no = int(difference.days * 24 + difference.seconds / 3600) + (16 * years) - hours_less
+        columns = ["Date", "Hour"] + column_list
+        path = "../../data/input/combined/all_data_hourly.csv"  # only works when run from src
+        if len(column_list) > 0:
+            dataset = pd.read_csv(path, skiprows=range(1, skip_rows_no), usecols=columns)
+        else:
+            dataset = pd.read_csv(path, skiprows=range(1, skip_rows_no))
+        dataset["Date"] = pd.to_datetime(dataset["Date"], format='%Y.%m.%d')
+        dataset = dataset[dataset['Date'] <= np.datetime64(last_date.date())]
+        return dataset
+    else:
+        assert False
+
+
+def check_valid_dates(from_date, to_date):  # helping method checking that dates are valid
+    valid = True
+    try:
+        first_date = datetime.datetime.strptime(from_date, '%d.%m.%Y')
+        last_date = datetime.datetime.strptime(to_date, '%d.%m.%Y')
+        if last_date < first_date:
+            print("First date must come before last date")
+            valid = False
+    except Exception as e:
+        print(e)
+    finally:
+        return valid
+
+
+if __name__ == '__main__':
+    date_string_from = "01.01.2019"
+    date_string_do = "31.12.2019"
+    col = ["System Price"]
+    data = get_data(date_string_from, date_string_do, col)
+    print(data)
---
+++ src/system/generate_periods.py
@@ -0,0 +1,125 @@
+# script for generating validation periods
+from data import data_handler
+import pandas as pd
+import math
+import datetime as dt
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_seasons():
+    winter = ("07.01.2019", "31.03.2019")
+    spring = ("01.04.2019", "30.06.2019")
+    summer = ("01.07.2019", "29.09.2019")
+    fall = ("30.09.2019", "29.12.2019")
+    return [winter, spring, summer, fall]
+
+
+def get_four_periods_median_method(write_summary):
+    periods = []
+    seasons = get_seasons()
+    for s in seasons:
+        period_table = pd.DataFrame(columns=["Start date", "End date", "Mean", "Variance", "Mean Dev", "Var Dev",
+                                             "Norm Mean Dev", "Norm Var Dev", "Score"])
+        data = data_handler.get_data(s[0], s[1], ["System Price"])
+        first_date_in_season = data.iloc[0]["Date"].date()
+        last_date_in_season = data.iloc[-1]["Date"].date()
+        number_of_periods = math.floor((len(data) / 24) / 14)
+        if write_summary:
+            print("First date in season: {}".format(first_date_in_season))
+            print("Last date in season: {}".format(last_date_in_season))
+            print("Number of periods in season: {}\n".format(number_of_periods))
+        for i in range(number_of_periods):
+            first_date = first_date_in_season + dt.timedelta(days=14 * i)
+            last_date = np.datetime64(first_date + dt.timedelta(days=13))
+            first_date = np.datetime64(first_date)
+            period = (data['Date'] >= first_date) & (data['Date'] <= last_date)
+            period_data = data[period]
+            period_mean = period_data["System Price"].mean()
+            period_variance = period_data.var()["System Price"]
+            row = {"Start date": first_date, "End date": last_date, "Mean": period_mean, "Variance": period_variance}
+            period_table = period_table.append(row, ignore_index=True)
+
+        season_mean = period_table["Mean"].mean()
+        season_variance = period_table["Variance"].mean()
+
+        for index, period in period_table.iterrows():
+            period_mean_dev = abs(period["Mean"] - season_mean)
+            period_var_dev = abs(period["Variance"] - season_variance)
+            period_table.at[index, "Mean Dev"] = period_mean_dev
+            period_table.at[index, "Var Dev"] = period_var_dev
+
+        mean_dev_mean = period_table["Mean Dev"].mean()
+        mean_dev_var = period_table["Var Dev"].mean()
+
+        for index, period in period_table.iterrows():
+            norm_dev_mean = abs(period["Mean Dev"] / mean_dev_mean)
+            norm_dev_var = abs(period["Var Dev"] / mean_dev_var)
+            score = norm_dev_var + norm_dev_mean
+            period_table.at[index, "Norm Mean Dev"] = norm_dev_mean
+            period_table.at[index, "Norm Var Dev"] = norm_dev_var
+            period_table.at[index, "Score"] = score
+
+        period_table = period_table.sort_values(by=["Score"], ignore_index=True)
+        start_date_optimal_period = period_table.at[0, "Start date"].date()
+        end_date_optimal_period = period_table.at[0, "End date"].date()
+        periods.append((start_date_optimal_period, end_date_optimal_period))
+
+        if write_summary:
+            print(period_table)
+            print("Optimal start: {}, optimal end: {}".format(start_date_optimal_period, end_date_optimal_period))
+            print("\n")
+
+    print("The following four periods are suggested as validation test periods")
+    for p in periods:
+        print(p)
+    print("\n")
+
+    return periods
+
+
+# method for plotting the periods during the year
+def plot_price_year_and_periods(periods, write_dates):
+    print("--Plotting validation periods for 2019--\n")
+    price_color = "steelblue"
+    per_color = "firebrick"
+    label_pad = 12
+    title_pad = 20
+    data = data_handler.get_data("01.01.2019", "31.12.2019", ["System Price"])
+    fig, ax = plt.subplots(figsize=(13, 5.5))
+    plt.plot(data["Date"], data["System Price"], label="SYS", color=price_color)
+    period = 1
+    for p in periods:
+        mask = (data['Date'] >= np.datetime64(p[0])) & (data['Date'] <= np.datetime64(p[1]))
+        period_data = data[mask]
+        plt.plot(period_data["Date"], period_data["System Price"], color=per_color, label="Per. {}".format(period))
+        period += 1
+    for line in plt.legend(loc='upper center', ncol=5, bbox_to_anchor=(0.5, 1.05),
+                           fancybox=True, shadow=True).get_lines():
+        line.set_linewidth(2)
+    for s in get_seasons():
+        if get_seasons().index(s) == 0:
+            start_date = dt.datetime.strptime(s[0], '%d.%m.%Y')
+            plt.axvline(start_date, linewidth=1, color='black', linestyle="--")
+        end_date = dt.datetime.strptime(s[1], '%d.%m.%Y')
+        plt.axvline(end_date, linewidth=1, color='black', linestyle="--")
+    if write_dates:
+        plt.ylim(0, max(data["System Price"])*1.12)
+        for i in range(len(periods)):
+            p = periods[i]
+            start_string = p[0].strftime("%d %b")
+            end_string = p[1].strftime("%d %b")
+            date_string = start_string + " - " + end_string
+            plt.text(i*0.22+0.12, 0.92, date_string, transform = ax.transAxes, color=per_color)
+    plt.ylabel("System price [€]", labelpad=label_pad)
+    plt.xlabel("Date", labelpad=label_pad)
+    plt.title("Validation Periods", pad=title_pad)
+    plt.tight_layout()
+    out_path = "../../data/output/plots/random/val_periods_median_method.png"
+    plt.savefig(out_path)
+    plt.close()
+
+
+if __name__ == '__main__':
+    periods_ = get_four_periods_median_method(write_summary=False)
+    plot_price_year_and_periods(periods_, write_dates=True)
