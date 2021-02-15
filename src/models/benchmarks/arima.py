191a5b256fdb9b597b8f48e300cf1eaaa059f2cb
Marius Engen <mariueng@stud.ntnu.no>
2021-02-15 17:03:00 +0100

    basic ARIMA done

--- src/models/benchmarks/arima.py
+++ src/models/benchmarks/arima.py
@@ -1,33 +1,54 @@
 import os
-from data import data_handler
 from src.models.model_handler import *
-from data import data_formatter
+from data import data_formatter, data_handler
+from src.system.metrics import evaluate_point_and_interval_forecast
 import pmdarima as pm
-import pandas as pd
 import numpy as np
-import pickle
-
-start_time = "01.01.2018"
-end_time = "31.01.2018"
-
-data = data_handler.get_data(start_time, end_time, ["System Price"], os.getcwd())
-data = data_formatter.combine_hour_day_month_year_to_datetime_index(data)
-split = 150
-train, test = data[:split], data[split:]
-
-# TODO: Try removing seasonal and m=12? It is a SARIMA model atm
-arima_model = pm.auto_arima(train, seasonal=True, m=12)
-
-# Forecast
-forecast, conf_int = arima_model.predict(test.shape[0], return_conf_int=True)
-# Combine results into one array
-results = np.column_stack((forecast, conf_int))
-# Add datetime period as index for forecast data
-forecast = data_formatter.array_to_dataframe_with_datetime_as_index(results, test)
-
-scores = dict()
 
 
+# Get data
+def load_data(start_time, end_time):
+    df = data_handler.get_data(start_time, end_time, ['System Price'], os.getcwd())
+    df = data_formatter.combine_hour_day_month_year_to_datetime_index(df)
+    return df
 
-# Save model
-# save_pickle(1, "arima", arima_model, forecast, scores, "validation")
+
+# Fit new model to new data
+def fit(train, verbose=False):
+    arima_model = pm.auto_arima(train)
+    if verbose:
+        print(arima_model.summary())
+    return arima_model
+
+
+# Use model to predict
+def predict(arima_, test):
+    forecast, conf_int = arima_.predict(test.shape[0], return_conf_int=True)
+    # Combine results into one array
+    results = np.column_stack((forecast, conf_int))
+    # Add datetime period as index for forecast data
+    forecast = data_formatter.point_and_intervals_to_dataframe_with_datetime_as_index(results, test)
+    return forecast
+
+
+def score(forecast, actual, train, nominal_coverage=0.95):
+    return evaluate_point_and_interval_forecast(forecast, actual, train, nominal_coverage)
+
+
+# Save model to file
+def save_model(id_, name, model, forecast, scores, folder):
+    save_pickle(id_, name, model, forecast, scores, folder)
+
+
+if __name__ == '__main__':
+    """
+    start_time_ = "01.01.2018"
+    end_time_ = "31.01.2018"
+    data_ = load_data(start_time_, end_time_)
+    split_ = 150
+    train_, test_ = data_[:split_], data_[split_:]
+    model_ = fit(train_)
+    forecast_ = predict(model_, test_)
+    scores = score(forecast_, test_, train_)
+    print(scores)
+    """
