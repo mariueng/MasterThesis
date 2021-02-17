4a9eab30f0c83fb9c76afb0763193b4f93ef1e14
Marius Engen <mariueng@stud.ntnu.no>
2021-02-17 11:47:54 +0100

    model superclass added

---
+++ src/models/benchmarks/model.py
@@ -0,0 +1,20 @@
+from abc import ABCMeta, abstractmethod
+from datetime import datetime as dt
+import pandas as pd
+
+
+class Model(metaclass=ABCMeta):
+    def __init__(self, name: str, df: pd.DataFrame):
+        self.name = name
+        self.creation_time = dt.today().strftime('%Y-%m-%d_%H:%M')
+        self.model = None
+
+    def get_name(self):
+        return self.name
+
+    def get_time(self):
+        return self.creation_time
+
+    @abstractmethod
+    def forecast(self, df: pd.DataFrame):
+        pass
