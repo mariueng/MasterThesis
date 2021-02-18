dd620751567b6ceeb167245f06f866a2c563a039
Marius Engen <mariueng@stud.ntnu.no>
2021-02-18 21:02:41 +0100

    minor fix in abstract superclass

--- src/models/benchmarks/model.py
+++ src/models/benchmarks/model.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 class Model(metaclass=ABCMeta):
-    def __init__(self, name: str, df: pd.DataFrame):
+    def __init__(self, name: str):
         self.name = name
         self.creation_time = dt.today().strftime('%Y-%m-%d_%H:%M')
         self.model = None
