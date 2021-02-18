de58c7b7087b616b785bb8df4bd56548bbb45bd9
Marius Engen <mariueng@stud.ntnu.no>
2021-02-18 21:01:38 +0100

    added directory maker

--- src/models/model_handler.py
+++ src/models/model_handler.py
@@ -2,6 +2,7 @@
 
 import os
 import pickle
+import shutil
 
 """
 Format that models are saved in:
@@ -17,23 +18,29 @@ Descriptions:
     then put in the corresponding folder.
 """
 
+
 def save_pickle(id_, name, model, forecast, scores, folder):
-    filepath = os.getcwd()
-    filepath = filepath[:len(filepath) - 18]
-    print(filepath)
-    # Final path should look like:
-    # '{workspace}$src\\results\\validation or test\\from-date_to-date\\'
-    # File should look like:
-    # 'name_id'
+    #filepath = os.getcwd()
+    #filepath = filepath[:filepath.index("src")]
     start_date = forecast.index[0].strftime('%Y-%m-%d')
     end_date = forecast.index[-1].strftime('%Y-%m-%d')
-    filepath += '\\results\\' + folder + '\\' + start_date + '_' + end_date + '\\' + name + '_' + str(id_) + ".pkl"
+    filepath = get_result_folder(model) + '\\' + str(id_) + '_' + start_date + '_' + end_date + '.pkl'
     tuple_model = (id_, name, model, forecast, scores, folder)
     with open(filepath, 'wb') as file:
         pickle.dump(tuple_model, file)
+
 
 def load_pickle(filepath):
     with open(filepath, 'rb') as file:
         tuple_model = pickle.load(file)
 
     return tuple_model
+
+
+def get_result_folder(model):
+    creation_time = ''  # To avoid overwriting, change to: '_' + model.get_time()
+    folder_path = '../results/validation' + (model.get_name() + creation_time).replace(' ', '')
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+    os.makedirs(folder_path)
+    return folder_path
